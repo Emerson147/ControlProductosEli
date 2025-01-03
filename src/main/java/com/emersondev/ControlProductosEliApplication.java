c1f4c2688a0cbeac29451d461d4ac33ba1e7e85d
Emerson <emersontec147@gmail.com>
2025-01-02 23:23:42 -0500

    Initial commit

---
+++ src/main/java/com/emersondev/ControlProductosEliApplication.java
@@ -0,0 +1,13 @@
+package com.emersondev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlProductosEliApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ControlProductosEliApplication.class, args);
+  }
+
+}
